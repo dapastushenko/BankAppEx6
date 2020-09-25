09d005794f72190b9113374682162b5a3da6a3bb
oyushche <uhmateush@gmail.com>
2020-09-25 05:40:11 -0400

    ex3 t1 finnished

--- src/main/java/BankApplication.java
+++ src/main/java/BankApplication.java
@@ -7,8 +7,12 @@ import com.luxoft.bankapp.service.BankReportService;
 import com.luxoft.bankapp.service.Banking;
 import com.luxoft.bankapp.model.Client.Gender;
 import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
+@ComponentScan("com.luxoft.bankapp")
 public class BankApplication {
 
     private static final String[] CLIENT_NAMES =
@@ -17,8 +21,7 @@ public class BankApplication {
     public static void main(String[] args) {
 
         ApplicationContext context =
-                new ClassPathXmlApplicationContext("application-context.xml",
-                        "test-clients.xml");
+                new AnnotationConfigApplicationContext(BankApplication.class);
 
         Banking banking = initialize(context);
 
