ff03e4a68b8bce69b8924ed618daa8545498883f
oyushche <uhmateush@gmail.com>
2020-10-21 02:45:27 -0400

    ex4 task 2 finished (moved up)

--- src/test/java/com/luxoft/bankapp/BankApplicationTests1.java
+++ src/test/java/com/luxoft/bankapp/BankApplicationTests1.java
@@ -13,7 +13,6 @@ import com.luxoft.bankapp.service.demo.DemoBankInitializationService;
 import com.luxoft.bankapp.service.storage.ClientRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Profile;
 import java.lang.annotation.Annotation;
